84742b2e904577c20e7dd2c5823a3d0ddc4412b9
YukinaYuri <yukinayuri18@gmail.com>
2024-12-31 12:25:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelector("#tree").addEventListener("click", () => {
+    alert("The tree says: 'Work together to solve challenges!'");
+});
+
+document.querySelector("#golem").addEventListener("click", () => {
+    alert("The golem says: 'Prove your loyalty to get the second gem.'");
+});
